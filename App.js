0f6c8b00e1f02209d89bddae2e244ef64ab3bb49
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-07-28 15:07:31 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.textoTitulo}>PREVISÃO DO TEMPO DATENA</Text>
+        <View style={styles.imgs}>
+        <Image
+      style={styles.imgg}
+      source={{
+        uri: 'https://p2.trrsf.com/image/fget/cf/648/0/images.terra.com/2022/05/13/870515956-datena-2-1.jpg',
+      }}
+      />
+      </View>
+      </View>
+      <View style={styles.blocos}>
+        <Text style={styles.texto}>CIDADE:</Text>
+        <TextInput
+          placeholder='digita sua cidade fi'
+          style={styles.input}
+          
+        />
+      </View>
+      <View style={styles.blocos}>
+        <TouchableOpacity
+        style={styles.btn}
+        >
+          <Text style={styles.btnTexto}>BUSCAR</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +37,50 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textoTitulo: {
+    fontSize: 40,
+    textAlign: 'center',
+    color: '#4281F5',
+  },
+  imgg:{
+    width: 120,
+    height: 90,
+    margin: 5,
+    borderRadius: 20,
+  },
+  imgs: {
+    alignItems: 'center',
+  },
+  blocos: {
+    fontSize: 20,
+  },
+  texto: {
+    color: '#4281F5',
+    fontSize: 20,
+    marginTop: '4%',
+    textAlign: 'center'
+  },
+  input: {
+    borderBottomWidth: 2,
+    width: 265,
+    height: 35,
+    fontSize: 20,
+    borderColor: '#4281F5'
+  },
+  btn: {
+    width: 265,
+    height: 45,
+    backgroundColor: '#4281F5',
+    alignItems: 'center',
+    margin: 10,
+    borderRadius: 5,
+  },
+  btnTexto: {
+    fontSize: 30,
+    color: '#FFF'
+  }
 });
---
+++ Components/previsao.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { styles } from './styles';
+
+export function Components() {
+  return (
+    <View style={styles.container}>
+        <Text></Text>
+    </View>
+  );
+}
