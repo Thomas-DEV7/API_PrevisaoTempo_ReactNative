d02a6b45325fa757787458052bdfc60da92e6355
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-09-15 14:28:19 -0300

    FUNCIONANDO, UFA DEUS!

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity,ScrollView } from 'react-native';
 import { Tempo } from './Components/previsao';
 import Api from './Components/Api';
 
@@ -8,7 +8,7 @@ export default function App() {
 
   const [cidade, setCidade] = useState('São Paulo')
   async function carregaDados(){
-    const response = await Api.get(`weather?array_limit=1&fields=only_results,temp,city_name,description,time,wind_speedy,forecast,max,min,date&key=7e7569c9&city_name=${cidade},SP`)
+    const response = await Api.get(`weather?array_limit=1&fields=only_results,temp,city_name,description,time,wind_speedy,forecast,max,min,date&key=cd8b7e55&city_name=${cidade},SP`)
     setDados(response.data);
   
   }
@@ -17,22 +17,23 @@ export default function App() {
     setCidade("");
   } 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <View>
-        <Text style={styles.textoTitulo}>PREVISÃO DO TEMPO DATENA</Text>
+        
         <View style={styles.imgs}>
         <Image
       style={styles.imgg}
       source={{
-        uri: 'https://p2.trrsf.com/image/fget/cf/648/0/images.terra.com/2022/05/13/870515956-datena-2-1.jpg',
+        uri: 'https://uploads.metropoles.com/wp-content/uploads/2022/02/28203715/Ratinho-4.jpg',
       }}
       />
+      <Text style={styles.tittle}>RATINHO TEMPOS</Text>
       </View>
       </View>
       <View style={styles.blocos}>
         <Text style={styles.texto}>CIDADE:</Text>
         <TextInput
-          placeholder='digita sua cidade fi'
+          placeholder='nome de sua cidade'
           style={styles.input}
           value={cidade}
           onChangeText={(cidade) => setCidade(cidade)}          
@@ -47,7 +48,7 @@ export default function App() {
         </TouchableOpacity>
 
         <TouchableOpacity
-        style={styles.btn}
+        style={styles.btnLimpar}
         onPress={limpar}
         >
           <Text style={styles.btnTexto}>LIMPAR</Text>
@@ -57,51 +58,50 @@ export default function App() {
       <View style={styles.blocos}>
         <Tempo  data={dados}/>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#FFF',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  textoTitulo: {
+  tittle: {
     fontSize: 40,
     textAlign: 'center',
-    color: '#4281F5',
+    color: 'purple',
   },
   imgg:{
-    width: 120,
-    height: 90,
-    margin: 5,
-    borderRadius: 20,
+    width: '100%',
+    height: 300
   },
   imgs: {
     alignItems: 'center',
   },
   blocos: {
     fontSize: 20,
+    alignItems: 'center'
   },
   texto: {
-    color: '#4281F5',
+    color: 'purple',
     fontSize: 20,
     marginTop: '4%',
     textAlign: 'center'
   },
   input: {
     borderBottomWidth: 2,
-    width: 265,
+    width: '90%',
     height: 35,
     fontSize: 20,
-    borderColor: '#4281F5'
+    borderColor: 'purple'
   },
   btn: {
-    width: 265,
+    width: '90%',
     height: 45,
-    backgroundColor: '#4281F5',
+    backgroundColor: 'purple',
     alignItems: 'center',
     margin: 10,
     borderRadius: 5,
@@ -109,5 +109,13 @@ const styles = StyleSheet.create({
   btnTexto: {
     fontSize: 30,
     color: '#FFF'
+  },
+  btnLimpar:{
+    width: '90%',
+    height: 45,
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    margin: 10,
+    borderRadius: 5,
   }
 });
